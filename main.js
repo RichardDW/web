d816d84174758ee9462de085bea6f02d54816db9
RichardDW <richard.dewolff@gmail.com>
2020-03-18 13:05:19 +0100

    Updates

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log('Hello world');
+console.error('This is an error');
+
+// var, let, const
+// var not to be used
+
+//let age = 31;
+
+//console.log(age - 4);
+
+// String, numbers, boolean, null, undefined
+
+const name = 'Richard'
+const age = 66;
+const isCool = true;
+const rating = 4.5;
+const x = null;
+
+let z // z = undefined
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof x);
+console.log(typeof z);
+
+
+
